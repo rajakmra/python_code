2bd5c2e5b99706de91d1ae917a7c5def11ecfbc1
Raja Kumar <rkumardgl@gmail.com>
2020-12-03 18:56:48 +0530

    ex01 is added

---
+++ ex01_perform_arithmetic_operations.py
@@ -0,0 +1,11 @@
+# Program to perform the arithmetic operations for the given 2 numbers
+# 11-11-2020
+# Raja Kumar. C
+num1 = 10
+num2 = 5
+print('num1 = ',num1)
+print('num2 = ',num1)
+print('Addition : ',num1+num2)
+print('Substraction : ',num1-num2)
+print('Multiplication : ',num1*num2)
+print('Divition : ',num1/num2)
